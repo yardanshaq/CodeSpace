dea759339d3e226c5e5415509e6030abb7b1c315
Yardan <lettaailsa@gmail.com>
2026-03-05 19:43:03 +0700

    change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "https://cdn.nekohime.site/file/1_6Wo0X-.png" },
+      { url: "https://cdn.nekohime.site/file/sOyPp0Jp.png" },
       { url: "https://cdn.nekohime.site/file/nMZIBy7P.png", sizes: "16x16", type: "image/png" },
       { url: "https://cdn.nekohime.site/file/dtbHw_6g.png", sizes: "32x32", type: "image/png" },
     ],
